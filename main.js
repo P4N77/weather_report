ff3edc473bed5d918642856714e27eacf8320efe
P4N77 <germanm1853@gmail.com>
2023-02-08 21:44:59 -0500

    reporte del clima funcionando

---
+++ main.js
@@ -0,0 +1,59 @@
+const resultadopais = document.querySelector('.resultado');
+const formulario = document.querySelector('.obtener_clima');
+const ciudad = document.querySelector('#ciudad');
+
+
+formulario.addEventListener('submit', (parametro) => {
+    parametro.preventDefault();
+    if (ciudad==="400") {
+        alert("no a ingresado una ciudad")
+    }else
+    buscarCiudadPais(ciudad.value);
+})
+
+function buscarCiudadPais(ciudad){
+    const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiId}`;
+
+    fetch(url)
+        .then(datos => {
+            return datos.json();
+        })
+        .then(datosJ => {
+            if (datosJ.cod === '404') {
+                alert("no se encontro la ciudad")
+            } else {
+                limpiar();
+                mostrarClima(datosJ);
+            }
+
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+function mostrarClima(e){
+    const {name, main:{temp,}, weather:[arr]} = e;
+
+    const degrees = kelvinToCentigrade(temp);
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+        <h2 class="clima">Clima en ${name}</h2>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+        <h2 class="clima">${degrees}°C</h2>
+    `;
+
+    resultadopais.appendChild(content);
+
+}
+
+
+function kelvinToCentigrade(temp){
+    return parseInt(temp - 273.15);
+}
+
+function limpiar(){
+    resultadopais.innerHTML = '';
+}
